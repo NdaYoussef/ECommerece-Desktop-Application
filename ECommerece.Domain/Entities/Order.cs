bdc538fad048129e96e96648bfeed479b9027c8e
Hajar Ismail <hageresmail1911@gmail.com>
2026-02-21 22:04:09 +0200

    Add Order entity, enums, and EF Core configuration
    Introduced the Order entity with properties for user, status, payment, shipping, and discount code. Added OrderStatus and PaymentStatus enums for tracking order and payment states. Created OrderConfigurations for EF Core mapping, including property constraints and commented relationship mappings. These changes establish the foundation for order management in the e-commerce domain.

---
+++ ECommerece.Domain/Entities/Order.cs
@@ -0,0 +1,60 @@
+﻿using ECommerece.Domain.BaseModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ECommerece.Domain.Enums;  
+namespace ECommerece.Domain.Entities
+{
+    public class Order :BaseClass<int>
+    {
+        public DateTime OrderDate { get; set; }= DateTime.Now;
+
+        public int UserId { get; set; }=0;
+        public decimal TotalAmount { get; private set; } = 0;
+
+        public OrderStatus Status { get; private set; }= OrderStatus.Pending;
+          public User User { get; set; }=null!;
+        //public List<OrderItem> OrderItems { get; set; }= new List<OrderItem>();
+
+        public decimal ShippingCost { get; set; } = 0;
+         public string ShippingAddress { get; set; } = string.Empty;
+        public PaymentStatus paymentstatus { get; set; } = PaymentStatus.Unknown;
+        public string discountcode { get; set; } = string.Empty;
+        //public decimal CalculateTotalAmount()
+        //{
+        //    decimal total = 0;
+        //    foreach (var item in OrderItems)
+        //    {
+        //        total += item.Quantity * item.UnitPrice;
+
+        //    }
+        //    total += ShippingCost;
+        //    if (!string.IsNullOrEmpty(discountcode))
+        //    {
+        //        // Apply discount 
+        //        total *= 0.9m; // Apply a 10% discount
+        //    }
+        //    return total;
+        //}
+         
+         public OrderStatus setOrerstaus()
+        {
+            if (Status == OrderStatus.Pending)
+            {
+                if (paymentstatus == PaymentStatus.Completed)
+                    Status = OrderStatus.Processing;
+            }
+            else if (Status == OrderStatus.Processing)
+            {
+                Thread.Sleep(2000); // Simulate processing time
+                Status = OrderStatus.Shipped;
+            }
+            else if (Status == OrderStatus.Shipped)
+            {
+                Thread.Sleep(3000); // Simulate delivery time
+                Status = OrderStatus.Delivered;
+            }
+            return Status;
+        }
+    }
+}
