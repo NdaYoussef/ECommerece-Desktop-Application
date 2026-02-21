cce6ee69ab815a5c482a1ef1503e2c0d5877db26
Eslam Abd-Elsatar <eslamlegend5@gmail.com>
2026-02-21 15:24:18 +0200

    check merge 03

--- ECommerece.Application/IRepositories/IProductRepository.cs
+++ ECommerece.Application/IRepositories/IProductRepository.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ECommerece.Application.Interfaces.Repositories;
 using ECommerece.Domain.Entities;
 
 namespace ECommerece.Application.IRepositories
