1a42c18619587ffc71b755d027144524ee8782e0
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-02-21 09:51:55 +0200

    add base class, category entity properties and project references configuration

---
+++ ECommerece.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using ECommerece.Domain.BaseModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Domain.Entities
+{
+    public class Category : BaseClass<int>
+    {
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        //public List<Product> products { get; set; }
+    }
+}
