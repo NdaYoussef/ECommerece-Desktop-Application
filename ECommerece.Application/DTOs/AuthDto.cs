b813bfffd5faa52791210ba40e629a4202fa6dcf
Nda Youssef <140206711+NdaYoussef@users.noreply.github.com>
2026-02-21 14:54:57 +0200

    Add Auth Dto Model to handle login & Register Featuers
    * add IRepos Folder
    
    * add IService Folder represent all service interfaces
    
    * add User file configurations
    
    * add csproj file
    
    * delete Interface folder and replace it with IService & IRepositories Folders
    
    * add User Dbset in context class
    
    * delete no longer needed gitKeep txt file
    
    * add auth Dto model to handle login & register featuers
    
    * add Iservice interface
    
    * add Iservice interface user edition

---
+++ ECommerece.Application/DTOs/AuthDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Application.DTOs
+{
+    public class AuthDto
+    {
+        public string? Message { get; set; }
+        public bool IsAuthenticated { get; set; } = false;
+        public object? Data { get; set; }
+    }
+}
---
+++ ECommerece.Application/IServices/IUserService/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ECommerece.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Application.IServices.IUserService
+{
+    public interface IUserService
+    {
+        Task<AuthDto> GetUserData(string userId);
+    }
+}
