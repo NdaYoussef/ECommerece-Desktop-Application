5b0377b6c04f767d1cc1c9a7c7a148483996ff6b
NdaYoussef <ndayou99@gmail.com>
2026-02-21 14:07:34 +0200

    add IRepos Folder

---
+++ ECommerece.Application/IRepositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Application.IRepositories
+{
+    public interface IGenericRepository<TEntity , Tkey> where TEntity : class
+    {
+        public IQueryable<TEntity> GetAll();
+        public TEntity GetById(Tkey id);
+        public void Add(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+
+    }
+}
