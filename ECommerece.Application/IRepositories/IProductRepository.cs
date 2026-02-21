7629bd98ed951ec7d6c6f7628b6c44c03b0a8211
Eslam-LC <148162542+Eslam-LC@users.noreply.github.com>
2026-02-21 18:27:01 +0200

    Feature/product entity (#7)
    * add product local
    
    * check after merge 01
    
    * move product dto
    
    * check after merge 02
    
    * check merge 03
    
    * fix annotation and dto

---
+++ ECommerece.Application/IRepositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerece.Domain.Entities;
+
+namespace ECommerece.Application.IRepositories
+{
+    public interface IProductRepository:IGenericRepository<Product,int>
+    {
+            
+            public Product GetProductByLabel(string label);
+            public bool SoftDelete(int id);
+    }
+}
---
+++ ECommerece.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerece.Domain.Entities
+{
+    public class Product : BaseModel.BaseClass<int>
+    {
+        public string? Label { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        // FK
+        public int CategoryId { get; set; }
+        // Navigation
+        public Category? Category { get; set; }
+
+        // Navigation
+        // public List<OrderItem>? OrderItems { get; set; }
+
+    }
+}
