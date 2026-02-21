ae17c2455eeed807f61f0b9964fa1c190a5e22e2
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-02-21 11:04:41 +0200

    add category entity configuration

--- ECommerece.Infrastructure/AppDbContext/AppDbContext.cs
+++ ECommerece.Infrastructure/AppDbContext/AppDbContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using ECommerece.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ECommerece.Infrastructure.AppDbContext
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        }
+
+       
     }
 }
---
+++ ECommerece.Infrastructure/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using ECommerece.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Infrastructure.Configurations
+{
+    internal class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.CategoryName).IsRequired().HasMaxLength(50);
+            builder.HasIndex(c => c.CategoryName).IsUnique();
+
+            builder.Property(c => c.CategoryDescription).IsRequired(false).HasMaxLength(200);
+           
+        }
+    }
+}
