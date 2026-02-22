f5c8d62b27bf84a36c599a623219ee353168c0fe
Shimaa Salah <shimaaasalah2002@gmail.com>
2026-02-22 12:17:42 +0200

    add ICategoryService interface

---
+++ ECommerece.Application/IServices/ICategoryService/ICategoryServices.cs
@@ -0,0 +1,17 @@
+﻿using ECommerece.Application.DTOs.CategoryDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Application.IServices.ICategoryService
+{
+    public interface ICategoryServices
+    {
+        public List<GetCategoryDto> GetAllCategories();
+        public GetCategoryDto GetCategoryById(int id);
+        public GetCategoryDto GetCategoryByName(string name);
+        public void AddCategory(AddCategoryDto CategoryDto);
+        public void UpdateCategory(UpdateCategoryDto CategoryDto);
+        public void DeleteCategory(int id);
+    }
+}
---
+++ ECommerece.Application/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using ECommerece.Application.DTOs.CategoryDto;
+using ECommerece.Application.IRepositories;
+using ECommerece.Application.IServices.ICategoryService;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Application.Services.CategoryServices
+{
+    public class CategoryService 
+    {
+        public ICategoryRepository _categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public List<GetCategoryDto> GetAllCategories()
+        {
+            var catList = _categoryRepository.GetAll().ToList();
+            var returnList = catList.Adapt<List<GetCategoryDto>>();
+            return returnList;
+        }
+
+    }
+}
