7676b0142a604c8c168d7a4c55ac034ac6385317
Hajar Ismail <hajarismail2030@gmail.com>
2026-02-22 12:42:06 +0200

    Add OrderItem entity and EF Core configuration (#11)
    * Add OrderItem entity and EF Core configuration
    
    Introduced the OrderItem entity in the domain layer with properties for order and product references, quantity, unit price, and a computed total price. Added corresponding EF Core configuration to set up the primary key and unit price precision. Relationship mappings to Product and Order are present but currently commented out.
    
    * Add navigation and config for OrderItem relationships
    
    Enabled navigation properties for Order and Product in OrderItem. Added computed TotalPrice property. Configured EF relationships for Order and Product with DeleteBehavior.Restrict.
    
    ---------
    
    Co-authored-by: Hajar Ismail <hageresmail1911@gmail.com>

---
+++ ECommerece.Infrastructure/Configurations/OrderItemconfigurations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ECommerece.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerece.Infrastructure.Configurations
+{
+    public class OrderItemconfigurations: IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(oi => oi.Id);
+            builder.Property(oi => oi.UnitPrice).IsRequired().HasColumnType("decimal(18,2)");
+
+            builder.HasOne(oi => oi.Product)
+                   .WithMany(p => p.orderitems)
+                   .HasForeignKey(oi => oi.ProductId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(oi => oi.Order)
+                   .WithMany(o => o.OrderItems)
+                   .HasForeignKey(oi => oi.OrderId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    
+    }
+}
