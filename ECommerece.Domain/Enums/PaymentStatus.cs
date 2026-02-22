859e674a6d311821579916792d653b81a1a87c7c
Hajar Ismail <hajarismail2030@gmail.com>
2026-02-22 11:41:16 +0200

    Add Order entity, enums, and EF Core configuration (#10)
    * Add Order entity, enums, and EF Core configuration
    
    Introduced the Order entity with properties for user, status, payment, shipping, and discount code. Added OrderStatus and PaymentStatus enums for tracking order and payment states. Created OrderConfigurations for EF Core mapping, including property constraints and commented relationship mappings. These changes establish the foundation for order management in the e-commerce domain.
    
    * Update Order entity: change UserId to string, adjust User
    
    Changed UserId from int to string and removed default value.
    Modified User property to remove non-nullable initialization.
    
    ---------
    
    Co-authored-by: Hajar Ismail <hageresmail1911@gmail.com>

---
+++ ECommerece.Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerece.Domain.Enums
+{
+    public enum PaymentStatus
+    {
+        Unknown = 0,
+        Pending = 1,
+        Completed = 2,
+        Failed = 3,
+        Refunded = 4,
+        Cancelled = 5
+
+    }
+}
