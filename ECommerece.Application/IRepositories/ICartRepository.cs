833f892bb4b3977f741f581200a84c7cd8ec3b87
Steven Ayman <stevenaymansalah@gmail.com>
2026-02-25 16:17:04 +0200

    add cart repositories,services

--- ECommerece.Application/IRepositories/ICartRepository.cs
+++ ECommerece.Application/IRepositories/ICartRepository.cs
@@ -7,11 +7,7 @@ namespace ECommerece.Application.IRepositories
 {
     public interface ICartRepository : IGenericRepository<Cart, int>
     {
-      //  public IQueryable<Cart> GetAllCart();
-     //   public Cart GetById(int id);
-
-    
-
+      
        Task ClearCart(int cartId);
     }
 }
--- ECommerece.Application/Mappers/Mapping.cs
+++ ECommerece.Application/Mappers/Mapping.cs
@@ -1,7 +1,6 @@
-﻿using ECommerece.Application.DTOs.OrderDtos;
-
+﻿using ECommerece.Application.DTOs.CartDto;
 ﻿using ECommerece.Application.DTOs.CategoryDto;
-
+using ECommerece.Application.DTOs.OrderDtos;
 using ECommerece.Application.DTOs.UserDto;
 using ECommerece.Domain.Entities;
 using Mapster;
@@ -25,6 +24,15 @@ namespace ECommerece.Application.Mappers
             TypeAdapterConfig< OrderItem, OrderItemDto>.NewConfig().TwoWays();
             TypeAdapterConfig<CreateOrderItemDto, OrderItem>.NewConfig();
 
+            TypeAdapterConfig<Cart, CartDTO>.NewConfig()
+                .Map(dest => dest.TotalAmount, src => src.CartItems.Sum(ci => ci.Quantity * ci.UnitPrice))
+                .Map(dest => dest.TotalItems, src => src.CartItems.Sum(ci => ci.Quantity))
+                .Map(dest => dest.Items, src => src.CartItems);
+
+            TypeAdapterConfig<CartItem, CartItemDTO>.NewConfig()
+                .Map(dest => dest.ProductName, src => src.Product.Label)
+                .Map(dest => dest.ProductImage, src => src.Product.ImageUrl)
+                .Map(dest => dest.TotalPrice, src => src.Quantity * src.UnitPrice);
 
             TypeAdapterConfig<Category, GetCategoryDto>.NewConfig()
                 .Map(dest => dest.ProductCount, src => src.Products != null ? src.Products.Count : 0);
